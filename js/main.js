38f6346ca53d2667087ecc033a151e79dc5924c2
umbertofaenza <umberto.faenza@gmail.com>
2023-08-23 16:15:19 +0200

    feat_js: add setInterval to refresh every second

--- js/main.js
+++ js/main.js
@@ -3,34 +3,32 @@ const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
-// get dates
-const today = new Date();
-const tomorrowCorrection = new Date("2023-08-24 9:30");
+setInterval(function () {
+  // get dates
+  const today = new Date();
+  const tomorrowCorrection = new Date("2023-08-24 9:30");
 
-// save dates as milliseconds since 1970
-const todayInMs = today.getTime();
-const tomorrowCorrectionInMs = tomorrowCorrection.getTime();
+  // save dates as milliseconds since 1970
+  const todayInMs = today.getTime();
+  const tomorrowCorrectionInMs = tomorrowCorrection.getTime();
 
-// find time remaining from now to Aug 24 2023 9:30am (in ms)
-const timeRemainingInMs = tomorrowCorrectionInMs - todayInMs;
+  // find time remaining from now to Aug 24 2023 9:30am (in ms)
+  const timeRemainingInMs = tomorrowCorrectionInMs - todayInMs;
 
-// find time remaining in seconds, minutes, hours, days
-const seconds = timeRemainingInMs / 1000;
-const secondsToDisplay = seconds % 60;
-console.log(Math.floor(secondsToDisplay));
+  // find time remaining in seconds, minutes, hours, days
+  const seconds = timeRemainingInMs / 1000;
+  const secondsToDisplay = seconds % 60;
 
-const minutes = seconds / 60;
-const minutesToDisplay = minutes % 60;
-console.log(Math.floor(minutesToDisplay));
+  const minutes = seconds / 60;
+  const minutesToDisplay = minutes % 60;
 
-const hours = minutes / 60;
-console.log(Math.floor(hours));
+  const hours = minutes / 60;
 
-const days = hours / 24;
-console.log(Math.floor(days));
+  const days = hours / 24;
 
-// print times on page
-secondsEl.innerText = Math.floor(secondsToDisplay);
-minutesEl.innerText = Math.floor(minutesToDisplay);
-hoursEl.innerText = Math.floor(hours);
-daysEl.innerText = Math.floor(days);
+  // print times on page
+  secondsEl.innerText = Math.floor(secondsToDisplay);
+  minutesEl.innerText = Math.floor(minutesToDisplay);
+  hoursEl.innerText = Math.floor(hours);
+  daysEl.innerText = Math.floor(days);
+}, 1000);
