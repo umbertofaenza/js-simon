1be077caa3682d9564180ac3550be74cab2b33fb
umbertofaenza <umberto.faenza@gmail.com>
2023-08-23 15:31:21 +0200

    feat_js: find time remaining in ms

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+// get dates
 const today = new Date();
 const tomorrowCorrection = new Date("2023-08-24 9:30");
 
+// save dates as milliseconds since 1970
 const todayInMs = today.getTime();
-console.log(todayInMs);
-
 const tomorrowCorrectionInMs = tomorrowCorrection.getTime();
-console.log(tomorrowCorrectionInMs);
+
+// find time remaining from now to Aug 24 2023 9:30am (in ms)
+const timeRemainingInMs = tomorrowCorrectionInMs - todayInMs;
