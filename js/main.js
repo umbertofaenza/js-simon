9c26015b63232f9cbf05bda4e268511e345b2863
umbertofaenza <umberto.faenza@gmail.com>
2023-08-23 15:48:00 +0200

    feat_js: find time in different formats

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@ const tomorrowCorrectionInMs = tomorrowCorrection.getTime();
 
 // find time remaining from now to Aug 24 2023 9:30am (in ms)
 const timeRemainingInMs = tomorrowCorrectionInMs - todayInMs;
+
+// find time remaining in seconds, minutes, hours, days
+const seconds = timeRemainingInMs / 1000;
+const secondsToDisplay = seconds % 60;
+console.log(Math.floor(secondsToDisplay));
+
+const minutes = seconds / 60;
+const minutesToDisplay = minutes % 60;
+console.log(Math.floor(minutesToDisplay));
+
+const hours = minutes / 60;
+console.log(Math.floor(hours));
+
+const days = hours / 24;
+console.log(Math.floor(days));
